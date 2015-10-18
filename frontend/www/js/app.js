ae7bc923adb3ab448d0a70b2a4791a197897d4f5
Jeremy Ko <jeremyko193@gmail.com>
2015-10-17 22:11:17 -0700

    Initial front end login and sign up

--- frontend/www/js/app.js
+++ frontend/www/js/app.js
@@ -6,8 +6,8 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform, $rootScope) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -20,9 +20,24 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
+
+  String.prototype.hashString = function() { 
+    var hash = 0, i, chr, len;
+    if (this.length == 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash.toString();
+  };
+
+  $rootScope.TOKEN = "";
+  $rootScope.SERVER_URL = "http://localhost:5000";
+  // $rootScope.SERVER_URL = "https://stormy-hollows-9187.herokuapp.com";
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
 
   .state('app', {
@@ -30,6 +45,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+
+  .state('sigup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'SignupCtrl'
   })
 
   .state('app.search', {
@@ -43,22 +70,23 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
+    url: '/browse',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/browse.html'
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
+    }
+  })
+
+  .state('app.playlists', {
+    url: '/playlists',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlists.html',
+        controller: 'PlaylistsCtrl'
       }
-    })
+    }
+  })
 
   .state('app.single', {
     url: '/playlists/:playlistId',
@@ -70,5 +98,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/login');
 });
--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,40 +8,63 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+})
 
-  // Form data for the login modal
+.controller('LoginCtrl', function ($scope, $state, $http, $ionicPopup) {
   $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    var requestData = {'login' : angular.copy($scope.loginData) };
+    requestData.login.password = requestData.login.password.hashString();
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    $http({
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      data: requestData,
+      url: this.$root.SERVER_URL + '/login'
+    }).then(function successCallback (response) {
+        console.log('Login success: auth token=' + response.data.token);
+        $scope.$root.TOKEN = response.data.token;
+        $state.go('app.search');
+    }, function errorCallback (response) {
+        $ionicPopup.alert({
+          title: 'Login Error',
+          template: response.data.errors
+        });
+    });
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
+.controller('SignupCtrl', function ($scope, $state, $http, $ionicPopup) {
+  $scope.signupData = {};
+
+  $scope.doSignup = function () {
+    var requestData = {'user' : angular.copy($scope.signupData) };
+    requestData.user.password = requestData.user.password.hashString();
+
+    $http({
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      data: requestData,
+      url: this.$root.SERVER_URL + '/users'
+    }).then(function successCallback (response) {
+        console.log('Create user success: auth token=' + response.data.token);
+        $scope.$root.TOKEN = response.data.token;
+        $state.go('app.search');
+    }, function errorCallback (response) {
+        $ionicPopup.alert({
+          title: 'Signup Error',
+          template: response.data.errors
+        });
+    });
+  };
+})
+
+.controller('PlaylistsCtrl', function ($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -52,50 +75,43 @@ angular.module('starter.controllers', [])
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PlaylistCtrl', function ($scope, $stateParams) {
 })
 
-.controller('SearchCtrl', function($scope, $ionicPopup, $http) {
+.controller('SearchCtrl', function ($scope, $ionicPopup, $http) {
   $scope.getTest = function () {
-      $http({
-        method: 'GET',
-        url: 'http://localhost:5000/widgets/1'
-      }).then(function successCallback (response) {
-          // this callback will be called asynchronously
-          // when the response is available
-          console.log(response.data.name);
-          var alertPopup = $ionicPopup.alert({
-              title: response.data.name,
-              template: 'CLICK ME. I KNOW YOU WANT TO!'
-          });
-          alertPopup.then(function (response) {
-              console.log('Button wuz clicked');
-          });
-      }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-      });
+    $http({
+      method: 'GET',
+      url: this.$root.SERVER_URL + '/widgets/1'
+    }).then(function successCallback (response) {
+        console.log(response.data.name);
+        var alertPopup = $ionicPopup.alert({
+          title: response.data.name,
+          template: 'CLICK ME. I KNOW YOU WANT TO!'
+        });
+        alertPopup.then(function (response) {
+          console.log('Button wuz clicked');
+        });
+    }, function errorCallback (response) {
+        console.log('Get error');
+    });
   };
 
   $scope.postTest = function (widget) {
-        var formData = angular.copy(widget);
-        formData = {'widget' : formData };
-        $http({
-          method: 'POST',
-          headers: {
-           'Content-Type': 'application/json'
-          },
-          data: formData,
-          url: 'http://localhost:5000/widgets'
-        }).then(function successCallback (response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            console.log('Post success');
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-            console.log('Post error');
-        });
-    };
+    var formData = angular.copy(widget);
+    formData = {'widget' : formData };
+    $http({
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      data: formData,
+      url: this.$root.SERVER_URL + '/widgets'
+    }).then(function successCallback (response) {
+        console.log('Post success');
+    }, function errorCallback (response) {
+        console.log('Post error');
+    });
+  };
 });
 
