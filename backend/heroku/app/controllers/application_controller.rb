947b79832b9b18b336612bb0829eea56fd49c1a7
William Zhang <william.zhang.285@gmail.com>
2015-10-17 23:20:41 -0700

    Only setting CSRF to null session if the request is in JSON format

--- backend/heroku/app/controllers/application_controller.rb
+++ backend/heroku/app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # TODO: WE WANT TO TURN THIS BACK ON
   respond_to :json
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
 end
