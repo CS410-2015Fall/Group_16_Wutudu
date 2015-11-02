cf6f3a83bb8b3239d1fb3281a88919e5b156eb0e
William Zhang <william.zhang.285@gmail.com>
2015-11-01 20:42:10 -0800

    Reverting the ssl change, and enabling the API keys to be read in production

--- backend/heroku/config/initializers/ssl_configuration.rb
+++ backend/heroku/config/initializers/ssl_configuration.rb
@@ -1,1 +1,3 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+if Rails.env.development? 
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
--- backend/heroku/config/initializers/third_party_api_setup_devl.rb
+++ backend/heroku/config/initializers/third_party_api_setup_devl.rb
@@ -1,14 +1,12 @@
 require 'yelp'
 
-if Rails.env.development? 
-  ENV['GOOGLE-API-KEY'] = "AIzaSyBQFqGYrIsaopElq94flPXuRmBAEPipwac"
-  ENV['FOUR-SQUARE-CLIENT-ID'] = "OAFMINVFDDMWB3MXW0RQMY4R0MYENMPH5V2JXJZQDNRQVM2N"
-  ENV['FOUR-SQUARE-CLIENT-SECRET'] = "0NVGFL135EYXAQXWIOSD5O30CSA1DC2CVR2IMLA1XYHOXXVY"
+ENV['GOOGLE-API-KEY'] = "AIzaSyBQFqGYrIsaopElq94flPXuRmBAEPipwac"
+ENV['FOUR-SQUARE-CLIENT-ID'] = "OAFMINVFDDMWB3MXW0RQMY4R0MYENMPH5V2JXJZQDNRQVM2N"
+ENV['FOUR-SQUARE-CLIENT-SECRET'] = "0NVGFL135EYXAQXWIOSD5O30CSA1DC2CVR2IMLA1XYHOXXVY"
 
-  Yelp.client.configure do |config|
-    config.consumer_key = 'RshqyqRhDD6JXHMAKMz7hw'
-    config.consumer_secret = 'zcXamZOi4_sVdmA_n5U2shJDfEk'
-    config.token = 'gYmf6RkQQMJY5sSXgLQbXLGLGNMvmvZ7'
-    config.token_secret = 'dWnqa4CLYh74Lc7IRQcKrm2cQww'
-  end
+Yelp.client.configure do |config|
+  config.consumer_key = 'RshqyqRhDD6JXHMAKMz7hw'
+  config.consumer_secret = 'zcXamZOi4_sVdmA_n5U2shJDfEk'
+  config.token = 'gYmf6RkQQMJY5sSXgLQbXLGLGNMvmvZ7'
+  config.token_secret = 'dWnqa4CLYh74Lc7IRQcKrm2cQww'
 end
