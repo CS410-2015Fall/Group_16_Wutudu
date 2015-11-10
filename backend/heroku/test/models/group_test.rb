1d644e4404575b74316ec9d3695ecbd7f76750dd
William Zhang <william.zhang.285@gmail.com>
2015-11-09 19:17:57 -0800

    Adding initial fixture files, and a test to group_test, while also adding a command in .travis.yml to load the fixtures before running the tests

--- backend/heroku/test/models/group_test.rb
+++ backend/heroku/test/models/group_test.rb
@@ -4,4 +4,7 @@ class GroupTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "group one has name" do
+    assert groups(:group_1).name = "Group One"
+  end
 end
