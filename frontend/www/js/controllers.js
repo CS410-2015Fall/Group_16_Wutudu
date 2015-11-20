7421c85e8d63b6bc14f750f49d6478bf15647055
Jeremy Ko <jeremyko193@gmail.com>
2015-11-20 00:38:17 -0800

    Add back button hack to always go to hamburger

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -1,13 +1,26 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function ($scope, $ionicPopup,
-  $httpService, $state, User, Auth) {
+  $httpService, $ionicHistory, $state, User, Auth) {
 
   $scope.$on('$ionicView.enter', init);
 
   $scope.logout = function () {
     Auth.logout()
       .then(logoutSuccess, handleError);
+  };
+
+  $scope.customBackClick = function () {
+    var historyId = $ionicHistory.backView().historyId;
+    historyId = (historyId) ? historyId : 'ion1';
+    var viewHistory = $ionicHistory.viewHistory();
+    if (!viewHistory.histories[historyId]) {
+      // Default back button functionality
+      $ionicHistory.goBack();
+    } else {
+      // Go back to the hamburger
+      $ionicHistory.goToHistoryRoot(historyId);
+    }
   };
 
   function init() {
